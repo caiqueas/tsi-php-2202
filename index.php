0ee434164a8366aee937cc7795c6c593a9d7aeb1
CAIQUE ARAUJO SILVA <caique.asilva17@senacsp.edu.br>
2022-08-24 22:00:19 -0300

    operadores, loopings e condicionais

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$nome = 'Caique';
-echo "Olá $nome";
+//$nome = 'Caique';
+//echo "Olá $nome";
 
 #echo 'Olá mundo!';
 //Comentário de linha
@@ -10,3 +10,64 @@ Comentário
 de
 bloco
 */
+
+//DOCUMENTAÇÃO DO PHP FICA EM: https://www.php.net
+
+/*
+$i = 1;
+for (; ; ) {
+    if ($i > 10) {
+        break;
+    }
+    echo "Caique <br>";
+    $i++;
+}
+*/
+
+/*
+$i = 1;
+while ($i <= 10) {
+    echo "Caique <br>";
+    $i++;  
+}
+*/
+
+/*
+$i = 0;
+do {
+    echo "Caique <br>";
+    $i++;
+} while ($i < 10);
+*/
+
+/*
+if (5 > 10){
+    echo "5 é maior que 10";
+}
+else{
+    echo "5 não é maior que 10";
+}
+*/
+
+$i = '2';
+
+switch ($i) {
+    case 0:
+        echo "Domingo";
+        break;
+    case 1:
+        echo "Segunda";
+        break;
+    case 2:
+        echo "Terça";
+        break;
+    case 3:
+        echo "Quarta";
+        break;
+    case 4:
+        echo "Quinta";
+        break;
+    case 5:
+        echo "Sexta";
+        break;
+}
